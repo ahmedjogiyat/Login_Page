d8d6c632cf700735553cad479e46ec80fe803a57
ahmed jogiyat <ahmedjogiyat123@gmail.com>
2020-07-13 11:19:15 +0500

    update .

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
        <NavBar/>
      
       <Paper elevation={3}  className ={classes.paper}> 
-              <h1>LogIn</h1>
+              <h1>LogInnnnnn</h1>
                <form className={classes.paper} noValidate autoComplete="off">
         <TextField  label="Enter Email" /> <br/><br/>
         <TextField  label="Enter Passsword" /> <br/><br/>
